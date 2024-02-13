f7b53a4a4af1db8e141497d128e110ae8c907f49
Addin Munawwar <moonawar@protonmail.com>
2024-02-13 22:52:47 +0700

    feat: with vertex buffer

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#define POS_ATTRIB 0
+#define COLOR_ATTRIB 1
+
 int main(void)
 {
     GLFWwindow* window;
@@ -13,7 +16,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "OpenGL Playground", NULL, NULL);
+    window = glfwCreateWindow(960, 720, "Hello Triangle", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -33,19 +36,32 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    float vertices[15] = 
+    {
+        -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f, 0.0f, 1.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, 15 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(POS_ATTRIB);
+    glVertexAttribPointer(POS_ATTRIB, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+    glEnableVertexAttribArray(COLOR_ATTRIB);
+    glVertexAttribPointer(COLOR_ATTRIB, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
