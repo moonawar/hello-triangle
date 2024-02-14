d728d3e029446c4304339b510b9c6b1d9433efbf
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-02-14 18:07:29 +0700

    feat: error utils poggies

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,28 @@
 #define POS_ATTRIB 0
 #define COLOR_ATTRIB 1
 
+// Error Handling
+#define ASSERT(x) if (!(x)) __builtin_trap();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char *function, const char *file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
+// Shader Linking
 struct ShaderProgramSource
 {
     std::string VertexShader;
